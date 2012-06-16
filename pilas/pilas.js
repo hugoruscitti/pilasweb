77e94cea468fa0fbe272eb23b3d79084b1f2b68e
Hugo Ruscitti <hugoruscitti@gmail.com>
2012-06-16 18:45:20 -0300

    creando un setter de prueba.

--- pilas/pilas.js
+++ pilas/pilas.js
@@ -44,9 +44,15 @@ var Texto = new Class({
         this.texto = texto
         this.imagen = new Text(this.texto, "22px arial")
         this.imagen.textBaseline = "top"
-        this.inicializar(x, y, centro_x, centro_y, escala_x, escala_y, rotacion);
+        this.inicializar(x, y, centro_x, centro_y, escala_x, escala_y, rotacion)
         pilas.agregar_actor(this)
     },
+
+});
+
+Texto.prototype.__defineSetter__("escala", function(valor) {
+    alert(valor)
+    this.escala_x = valor;
 });
 
 var Camara = new Class({
