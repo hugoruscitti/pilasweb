26d1dfa1ae10ff7c088d721bc6839cd72c8dbac9
Hugo Ruscitti <hugoruscitti@gmail.com>
2012-06-28 22:08:08 -0300

    Incorporando easeljs y box2d

--- pilasengine/main.js
+++ pilasengine/main.js
@@ -1,6 +1,12 @@
-define(['actores', 'mootools', 'utils'], function(actores, mootools, utils){
+define(['actores', 'mootools', 'utils', 'libs/easeljs-0.4.2.min', 'libs/Box2dWeb-2.1.a.3.min'], function(actores, mootools, utils, easel, box2d){
 
     var Pilas = new mootools.Class({
+
+        initialize: function(id_canvas){
+            this.canvas = document.id(canvas) 
+            this.stage = new Stage(this.canvas)
+        },
+
         utils: utils,
         actores: actores,
     });
--- tests/tests.js
+++ tests/tests.js
@@ -5,13 +5,12 @@ require(['require', './mocha.js'], function(require){
         console.log("module: ", pilasengine);
 
         it("inicializa correctamente", function() {
-            var pilas = new pilasengine.Pilas();
+            var pilas = new pilasengine.Pilas('canvas');
             console.log(pilas.utils.sumar(1, 2))
         });
 
-        it("detecta que existe el atributo actores", function() {
-            var pilas = new pilasengine.Pilas();
-            (2 +2).should.equal(3)
+        it("falla si no encuenta en canvas", function() {
+            var pilas = new pilasengine.Pilas('id_de_un_canvas_que_no_existe');
         })
 
 
