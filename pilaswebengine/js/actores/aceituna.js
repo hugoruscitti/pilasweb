df2deb316bf71509a46a96043d8851ab5d6e2bb5
sergio@sergiomini <sergiolindo.empresa@gmail.com>
2012-11-25 22:15:38 +0100

    Imágenes de actores en 'pilaswebengine/data/actores'.

--- pilaswebengine/js/actores/aceituna.js
+++ pilaswebengine/js/actores/aceituna.js
@@ -9,7 +9,7 @@ define(
       initialize: function(x, y, centro_x, centro_y, escala_x, escala_y, rotacion) {
         this.parent("sin_imagen.png", x, y, centro_x, centro_y, escala_x, escala_y, rotacion);
         this.nombre = "Aceituna";
-        this.imagen = this.cargar_imagen("aceituna.png");
+        this.imagen = this.cargar_imagen("actores/aceituna.png");
         this.centro_x = 18;
         this.centro_y = 18;
       },
