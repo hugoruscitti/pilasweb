3a95ef62eb20bf812090119735a6d9563215c733
sergio@sergiomini <sergiolindo.empresa@gmail.com>
2012-11-25 19:33:37 +0100

    Actores separados en ficheros. Debido a que pilas.js está muy lleno de módulos requeridos, se ha conservado 'pilaswebengine/js/actores.js' como recopilador de los actores dentro de 'pilaswebengine/js/actores/'.

--- pilaswebengine/js/actores.js
+++ pilaswebengine/js/actores.js
@@ -1,50 +1,10 @@
 define(
-  ['require', 'singleton', 'actores/actor', 'mootools'],
-  function(require, singleton, actor, mootools){
-    /**
-     * @extends Actor
-     * Representa una cadena de texto.
-     *
-     *      @example
-     *      var saludo = pilas.actores.Texto("Hola mundo", 30, 30);
-     *      saludo.rotacion = 30;
-     *
-     */
-    var Texto = new Class({
-      Extends: actor.Actor,
-      initialize: function(texto, x, y, centro_x, centro_y, escala_x, escala_y, rotacion) {
-        this.texto = texto;
-        this.nombre = "Texto";
-        this.imagen = new Text(this.texto, "22px arial");
-        this.imagen.textBaseline = "top";
-        this.inicializar(x, y, centro_x, centro_y, escala_x, escala_y, rotacion);
-        pilas.agregar_actor(this);
-      }
-    });
-
-    /**
-     * @extends Actor
-     */
-    var Aceituna = new Class({
-      Extends: actor.Actor,
-      initialize: function(x, y, centro_x, centro_y, escala_x, escala_y, rotacion) {
-        this.parent("sin_imagen.png", x, y, centro_x, centro_y, escala_x, escala_y, rotacion);
-        this.nombre = "Aceituna";
-        this.imagen = this.cargar_imagen("aceituna.png");
-        this.centro_x = 18;
-        this.centro_y = 18;
-      },
-      actualizar: function(){
-      },
-      click: function(){
-        this.eliminar();
-      }
-    });
-
+  ['require', 'singleton', 'actores/actor', 'actores/aceituna', 'actores/texto', 'mootools'],
+  function(require, singleton, actor, actor_aceituna, actor_texto, mootools){
     return {
       Actor: actor.Actor,
-      Aceituna: Aceituna,
-      Texto: Texto
+      Aceituna: actor_aceituna.Aceituna,
+      Texto: actor_texto.Texto
     };
   }
 );
---
+++ pilaswebengine/js/actores/aceituna.js
@@ -0,0 +1,27 @@
+define(
+  ['require', 'singleton', 'actores/actor', 'mootools'],
+  function(require, singleton, actor, mootools){
+    /**
+     * @extends Actor
+     */
+    var Aceituna = new Class({
+      Extends: actor.Actor,
+      initialize: function(x, y, centro_x, centro_y, escala_x, escala_y, rotacion) {
+        this.parent("sin_imagen.png", x, y, centro_x, centro_y, escala_x, escala_y, rotacion);
+        this.nombre = "Aceituna";
+        this.imagen = this.cargar_imagen("aceituna.png");
+        this.centro_x = 18;
+        this.centro_y = 18;
+      },
+      actualizar: function(){
+      },
+      click: function(){
+        this.eliminar();
+      }
+    });
+
+    return {
+      Aceituna: Aceituna
+    };
+  }
+);
