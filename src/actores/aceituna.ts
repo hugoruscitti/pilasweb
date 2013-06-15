813bf8c704e0eae25dbba1393bd05df2c467aacf
Hugo Ruscitti <hugoruscitti@gmail.com>
2013-06-15 17:51:11 -0300

    Creando los atributos de actores: centro_x , centro_y, rotacion, transparencia, escala_x y escala_y

--- src/actores/aceituna.ts
+++ src/actores/aceituna.ts
@@ -5,6 +5,8 @@ class Aceituna extends Actor {
   constructor() {
     var imagen = pilas.imagenes.cargar("aceituna.png");
     super(0, 0, imagen);
+    this.centro_x = 18;
+    this.centro_y = 18;
   }
 
 }
--- src/actores/actor.ts
+++ src/actores/actor.ts
@@ -16,6 +16,24 @@ class Actor {
   get y() {return this.sprite.y};
   set y(_y) {return this.sprite.y = _y};
 
+  get centro_x() {return this.sprite.regX};
+  set centro_x(_x) {this.sprite.regX = _x};
+
+  get centro_y() {return this.sprite.regY};
+  set centro_y(_y) {this.sprite.regY = _y};
+
+  get escala_x() {return this.sprite.scaleX};
+  set escala_x(_x) {this.sprite.scaleX = _x};
+
+  get escala_y() {return this.sprite.scaleY};
+  set escala_y(_y) {this.sprite.scaleY = _y};
+
+  get rotacion() {return this.sprite.rotation};
+  set rotacion(_r) {this.sprite.rotation = _r};
+
+  get transparencia() {return (-100 * this.sprite.alpha) + 100};
+  set transparencia(_t) {this.sprite.alpha = (_t - 100) / -100};
+
   actualizar() {
   }
 }
