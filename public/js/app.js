5bec170b3a4e276716c6268a216190b529f5184d
Hugo Ruscitti <hugoruscitti@gmail.com>
2013-06-28 17:14:54 -0300

    Implementando un editor básico.

--- public/js/app.js
+++ public/js/app.js
@@ -3,6 +3,7 @@ var app = angular.module('app', []);
 app.config(function($routeProvider) {
 
   $routeProvider.when('/', {redirectTo: '/pepe', controller: 'Controller'}).
+                 when('/editor', {templateUrl: 'editor.html', controller: 'EditorController'}).
                  when('/demo1', {templateUrl: 'demo1.html', controller: 'Controller'}).
                  when('/demo2', {templateUrl: 'demo2.html', controller: 'Controller'});
 
@@ -19,6 +20,7 @@ app.controller('AppController', function($scope, $location) {
   $scope.demos = [
     {'nombre': 'Aceituna y bomba', 'url': '#/demo1'},
     {'nombre': 'Escala', 'url': '#/demo2'},
+    {'nombre': 'Editor', 'url': '#/editor'},
   ];
 
   $scope.seleccionar = function(indice) {
@@ -31,5 +33,30 @@ app.controller('Controller', function($scope, $location) {
   $scope.iniciar_ejemplo = function(numero) {
     codigo = document.getElementById('/demo' + numero).innerHTML;
     eval(codigo);
+  }
+});
+
+app.controller('EditorController', function($scope, $location) {
+
+  $scope.codigo = "\
+pilas = new Pilas(); \n\
+pilas.iniciar({ancho: 320, alto: 240, data_path: 'data'}); \n\
+pilas.onready = function() { \n\
+  var fondo = new pilas.fondos.Plano(); \n\
+  aceituna = new pilas.actores.Aceituna(); \n\
+  //aceituna.escala = [2]; \n\
+} \n\
+\
+pilas.ejecutar();";
+
+  $scope.error = "";
+
+  $scope.ejecutar = function() {
+    try {
+      eval($scope.codigo);
+      $scope.error = "";
+    } catch (e) {
+      $scope.error = e.toString();
+    }
   }
 });
