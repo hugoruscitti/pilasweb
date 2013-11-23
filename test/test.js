bbd3a89c3f4cca7be06a21be4a6ae74fdce9c7bc
Hugo Ruscitti <hugoruscitti@gmail.com>
2013-11-23 20:15:26 -0300

    modificando los test para comenzar a repararlos.

--- test/test.js
+++ test/test.js
@@ -1,1 +1,17 @@
-expect(12).to.be.a('string');
+describe("Inicializar", function(){
+  it("Inicializa correctamente", function() {
+    pilas.should.be.ok;
+  });
+
+  it("Tiene que ser un número y no una cadena", function() {
+    var numero = 123;
+    numero.should.be.ok;
+  });
+
+  it("Tiene que dar error si no está el canvas", function() {
+    var pilas = new pilasengine.Pilas('id_de_un_canvas_que_no_existe');
+  }).should.throw(Error);
+
+
+});
+
