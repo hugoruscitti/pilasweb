27014b33dba82031459c7439a844e4352abf1d6c
Irving Rodriguez <irving.prog@gmail.com>
2013-12-04 12:35:11 -0600

    Clase Grupo para almacenar varios actores y tratarlos cómo si fueran uno solo

---
+++ src/grupo.ts
@@ -0,0 +1,80 @@
+//modulo grupo
+class grupo {
+	Grupo;
+	constructor() {
+		this.Grupo = Grupo;
+	}
+}
+
+class Grupo {
+	//TODO: encontrar una forma de llamar implicitamente a las funciones y propiedades de los actores
+	//		parecido a __getattr__ y __setattr__ de Python
+	lista;
+
+	constructor(lista) {
+		this.lista = lista;
+	}
+
+	get x() {
+		return this.__getattr__("x");
+	}
+
+	set x(x) {
+		this.__setattr__("x",x);
+	}
+
+	get y() {
+		return this.__getattr__("y");
+	}
+	
+	set y(y) {
+		this.__setattr__("y",y);
+	}
+
+	get escala() {
+		return this.__getattr__("escala");
+	}
+	
+	set escala(escala) {
+		this.__setattr__("escala",escala);
+	}
+
+	get rotacion() {
+		return this.__getattr__("rotacion");
+	}
+	
+	set rotacion(rotacion) {
+		this.__setattr__("rotacion",rotacion);
+	}
+
+	aprender(args, args2={}) {
+		this.__execfunct__("aprender", args, args2);
+	}
+
+	hacer(args, args2={}) {
+		this.__execfunct__("hacer", args, args2);
+	}
+
+	__execfunct__(id,args, args2) {
+		for(var i=0;i<this.lista.length;i++) {
+			this.lista[i][id](args, args2);
+		}
+	}
+
+	__getattr__(attr) {
+	
+		var valores = []
+		for(var i=0;i<this.lista.length;i++) {
+			valores.push(this.lista[i][attr]);
+		}
+
+		return valores;
+	}
+
+	__setattr__(attr, valor) {
+		for(var i=0;i<this.lista.length;i++) {
+			this.lista[i][attr] = valor;
+		}
+	}
+
+}
