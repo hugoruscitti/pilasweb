527a9423d33ee2d1f590be2b17b8c8265ab01518
Irving Rodriguez <irving.prog@gmail.com>
2013-12-11 01:18:07 -0600

    Nuevo actor Puntaje

---
+++ src/actores/puntaje.ts
@@ -0,0 +1,31 @@
+/// <reference path="actor.ts"/>
+/// <reference path="texto.ts"/>
+
+class Puntaje extends Texto {
+	valor;
+
+	constructor(x=0, y=0, puntaje=0, color='black') {
+		this.valor = puntaje;
+		super(x, y, this.valor.toString(), color);
+	}
+
+	aumentar(aumento) { 
+		this.valor += aumento;
+		this.texto = this.valor.toString();
+		
+		//Conservar la escala y el radio de colisión
+		//TODO: es necesario mejorar el actor Texto
+		var escala = this.escala;
+		var radio_de_colision = this.radio_de_colision;
+		
+		this.eliminar_texto();
+		this.crear_texto();
+		this.escala = escala;
+		this.radio_de_colision = radio_de_colision;
+	}
+
+	obtener() {
+		return this.valor;
+	}
+
+}
