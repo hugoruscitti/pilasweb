e9056a2b07719153dc72e2b27ef67149eab42e9e
irvingprog <irving.prog@gmail.com>
2014-03-13 15:22:33 -0600

    Refactorizando modos de Depuración

--- src/depurador.ts
+++ src/depurador.ts
@@ -53,11 +53,10 @@ class DepuradorDeshabilitado {
   }
 }
 
-
-
-class ModoRadiosDeColision {
+class ModoDeDepuracion {
   shape;
   container;
+  grosor_linea;
 
   constructor() {
     this.container = new createjs.Container();
@@ -66,6 +65,8 @@ class ModoRadiosDeColision {
     this.container.addChild(this.shape);
 
     pilas.escena_actual().stage.addChild(this.container)
+
+    this.grosor_linea = 2;
   }
 
   eliminar() {
@@ -73,70 +74,42 @@ class ModoRadiosDeColision {
   }
 
   actualizar() {
-    var escena = pilas.escena_actual();
     this.shape.graphics.clear();
+  }
+}
 
+class ModoRadiosDeColision extends ModoDeDepuracion {
+  actualizar() {
+    super.actualizar()
+    var escena = pilas.escena_actual()
     for (var i=0; i<escena.actores.length; i++) {
       var actor = escena.actores[i];
       var posicion = escena.obtener_posicion_pantalla(actor.x, actor.y);
 
-      this.shape.graphics.beginStroke("#FFF").drawCircle(posicion.x, posicion.y, actor.radio_de_colision).endStroke();
+      this.shape.graphics.beginStroke("#FFF").setStrokeStyle(this.grosor_linea).drawCircle(posicion.x, posicion.y, actor.radio_de_colision).endStroke();
 
     }
   }
 }
 
-class ModoArea {
-  shape;
-  container;
-
-  constructor() {
-    this.container = new createjs.Container();
-
-    this.shape = new createjs.Shape();
-    this.container.addChild(this.shape);
-
-    pilas.escena_actual().stage.addChild(this.container);
-  }
-
-  eliminar() {
-    pilas.escena_actual().stage.removeChild(this.container);
-  }
-
+class ModoArea extends ModoDeDepuracion {
   actualizar() {
+    super.actualizar()
     var escena = pilas.escena_actual();
-    this.shape.graphics.clear();
 
     for(var i=0;i<escena.actores.length;i++) {
       var actor = escena.actores[i];
       var posicion = escena.obtener_posicion_pantalla(actor.x, actor.y);
 
-      this.shape.graphics.beginStroke("#FFF").drawRect(posicion.x-actor.ancho/2, 
-        posicion.y-actor.alto/2, actor.ancho, actor.alto).endStroke();
+      this.shape.graphics.beginStroke("#FFF").setStrokeStyle(this.grosor_linea).drawRect(posicion.x-actor.ancho/2, posicion.y-actor.alto/2, actor.ancho, actor.alto).endStroke();
     }
   }
 }
 
-class ModoPuntosDeControl {
-  shape;
-  container;
-
-  constructor() {
-    this.container = new createjs.Container();
-
-    this.shape = new createjs.Shape();
-    this.container.addChild(this.shape);
-
-    pilas.escena_actual().stage.addChild(this.container);
-  }
-
-  eliminar() {
-    pilas.escena_actual().stage.removeChild(this.container);
-  }
-
+class ModoPuntosDeControl extends ModoDeDepuracion {
   actualizar() {
+    super.actualizar()
     var escena = pilas.escena_actual();
-    this.shape.graphics.clear();
 
     for (var i=0; i<escena.actores.length; i++) {
       var actor = escena.actores[i];
@@ -144,35 +117,34 @@ class ModoPuntosDeControl {
       var size = 3;
 
       // Dibuja una cruz
-      this.shape.graphics.beginStroke("#ffffff").moveTo(posicion.x - size, posicion.y - size).lineTo(posicion.x + size, posicion.y + size).endStroke();
-      this.shape.graphics.beginStroke("#ffffff").moveTo(posicion.x - size, posicion.y + size).lineTo(posicion.x + size, posicion.y - size).endStroke();
+      this.shape.graphics.beginStroke("#FFF").setStrokeStyle(this.grosor_linea).moveTo(posicion.x - size, posicion.y - size).lineTo(posicion.x + size, posicion.y + size).endStroke();
+      this.shape.graphics.beginStroke("#FFF").setStrokeStyle(this.grosor_linea).moveTo(posicion.x - size, posicion.y + size).lineTo(posicion.x + size, posicion.y - size).endStroke();
     }
   }
 
 }
 
+class ModoFisica extends ModoDeDepuracion {
+  actualizar() {
+    super.actualizar()
+    var escena = pilas.escena_actual();
+    this.shape.graphics.setStrokeStyle(this.grosor_linea);
+    escena.fisica.dibujar_figuras_sobre_lienzo(this.shape.graphics);
+  }
+}
 
-class ModoPosicion {
-  shape;
-  container;
+class ModoPosicion extends ModoDeDepuracion {
   text_coordenada;
   eje;
 
   constructor() {
-    this.container = new createjs.Container();
-
-    this.shape = new createjs.Shape();
-    this.container.addChild(this.shape);
-
+    super();
     this.text_coordenada = new createjs.Text("Posición del mouse: x=12 y=33", "12px Arial", "white");
     this.text_coordenada.y = 920/2; //TODO: Tamaño decanvas 640*480 
     this.text_coordenada.x = 900/2;
     this.container.addChild(this.text_coordenada);
     this.eje = new pilas.actores.Eje();
 		this.sobre_escribir_dibujado();
-
-
-    pilas.escena_actual().stage.addChild(this.container);
   }
 
 	private sobre_escribir_dibujado() {
@@ -198,14 +170,13 @@ class ModoPosicion {
 	}
 
   eliminar() {
-    pilas.escena_actual().stage.removeChild(this.container);
+    super.eliminar();
     this.eje.eliminar();
   }
 
   actualizar() {
-    var escena = pilas.escena_actual();
-    this.shape.graphics.clear();
-		
+    super.actualizar();
+    var escena = pilas.escena_actual();		
 		this.shape.graphics.actores = escena.actores;
 		this.shape.graphics.escena = escena;
 
@@ -215,40 +186,13 @@ class ModoPosicion {
       var size = 3;
 
       // Dibuja un punto
-      this.shape.graphics.beginFill("#ffffff").drawCircle(posicion.x, posicion.y, 2);
+      this.shape.graphics.beginFill("#FFF").setStrokeStyle(this.grosor_linea).drawCircle(posicion.x, posicion.y, 2);
     }
 
     var pos = escena.obtener_posicion_escenario(escena.stage.mouseX, escena.stage.mouseY);
     this.text_coordenada.text = "Posición del mouse: x=" + Math.floor(pos.x) + " y=" + Math.floor(pos.y);
   }
 
-}
-
-
-
-class ModoFisica {
-  shape;
-  container;
-
-  constructor() {
-    this.container = new createjs.Container();
-
-    this.shape = new createjs.Shape();
-    this.container.addChild(this.shape);
-
-    pilas.escena_actual().stage.addChild(this.container);
-  }
-
-  eliminar() {
-    pilas.escena_actual().stage.removeChild(this.container);
-  }
-
-  actualizar() {
-    var escena = pilas.escena_actual();
-    this.shape.graphics.clear();
-    this.shape.graphics.setStrokeStyle(3);
-    escena.fisica.dibujar_figuras_sobre_lienzo(this.shape.graphics);
-  }
 }
 
 
