d475574353214c1c37988858cab4dfc6f8a533b1
Lucas Alan Silvestri <silvestrilucasalan@gmail.com>
2019-03-18 14:47:33 -0300

    Globo no participa de los cambios en modo lectura

--- src/actores/globo.ts
+++ src/actores/globo.ts
@@ -56,7 +56,6 @@ class Globo extends Actor {
     }
 
     this.actor_texto = new Texto(x, y, this.mensaje, { z: z, anchoMaximo: this.anchoMaximo });
-    pilas.modoDeLectura().adaptarTexto(this.actor_texto);
     this.actor_texto.agregar_habilidad(ImitarPosicion, { objeto_a_imitar: this });
   }
 
